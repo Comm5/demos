b52b6f7aad09702e67c6bc8e46d8a049e07a82a3
Thiago A. Correa <thiago@comm5.com.br>
2014-02-14 16:18:52 -0200

    API para controle do terminal.

--- ma-xxxx/Python/acionamento.py
+++ ma-xxxx/Python/acionamento.py
@@ -2,30 +2,47 @@
 import socket
 
 class Acionamento:
-
+    
     def __init__(self):
-        self.__lastSensorRead = 0
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
+		self.__lastSensorRead = 0
+		self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.socket.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+    
     def connect(self, host):
         self.socket.connect((host, 5000))
-
+    
     def setRele(self, rele, estado):
         if ( estado ):
             self.socket.send("set %i\n" % rele)
         else:
             self.socket.send("reset %i\n" % rele)
-
+        
         buffer = ""
-        while( not buffer.contains("210") ):
+        while( not "210" in buffer ):
             buffer += self.socket.recv(1024)
-
+    
     def sensor(self):
-        pass
+		self.socket.send("query\n");
+		
+		buffer = ""
+		while( not "210" in buffer ):
+			buffer += self.socket.recv(1024)
+		
+		lastUsableLine = ""
+		lines = buffer.split('\n')
+		for line in reversed(lines):
+			if "210" in line:
+				lastUsableLine = line.replace('\r', '')
+				break
+		tokens = lastUsableLine.split(" ")
+		self.__lastSensorRead = int(tokens[1], 16);
+		
+		return self.__lastSensorRead
 
-    def isSensorActive(self):
-        pass
-
+    
+    def isSensorActive(self, sensor):
+        return ((self.__lastSensorRead & (1 << sensor)) != 0);
+    
     def disconnect(self):
-        pass
+        self.socket.close()
 
